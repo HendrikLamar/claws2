1727184266714e153435f7743f20ed37b9daf6bd
Hendrik Windel <hwindel@mpp.mpg.de>
2017-10-03 15:47:28 +0200

    Added runnummer counter and belonging functions

---
+++ include/storage.h
@@ -0,0 +1,39 @@
+// =====================================================================================
+// 
+//       Filename:  storage.h
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  03.10.2017 04:34:28
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Hendrik Windel (hw), hwindel@mpp.mpg.de
+//        Company:  Max Planck Institute for Physics
+//        License:  GPLv3 - 2017, Hendrik Windel 
+// 
+// =====================================================================================
+
+
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <TH1I.h>
+
+
+
+
+class Storage
+{
+    private:
+
+
+    public:
+
+};
+
+
+
+
+#endif // STORAGE_H
---
+++ src/storage.cxx
@@ -0,0 +1,21 @@
+// =====================================================================================
+// 
+//       Filename:  storage.cxx
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  03.10.2017 04:34:02
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Hendrik Windel (hw), hwindel@mpp.mpg.de
+//        Company:  Max Planck Institute for Physics
+//        License:  GPLv3 - 2017, Hendrik Windel 
+// 
+// =====================================================================================
+
+
+#include "storage.h"
+
+
